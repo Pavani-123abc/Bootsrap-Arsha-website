a4ab828babb88d04ba14d5bdd4117c9fbad11119
Pavani-123abc <saibhavanipanabaka@gmail.com>
2024-10-20 19:12:50 +0530

    first commit

---
+++ table.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+    $('.logos').slick({
+        // $('.autoplay').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
+//   });
+
+//  $(document).ready(function () {
+//      $('.tests').slick({
+//          slidesToShow: 1,
+//          slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//     });
+//  });
